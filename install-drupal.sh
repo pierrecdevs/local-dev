e90f3a08eb969daec6ed9b16a1cd9c6002c21c5e
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-02 16:15:29 -0400

    Updated the install script to include additional steps

--- install-drupal.sh
+++ install-drupal.sh
@@ -1,4 +1,6 @@
 #/bin/env bash
-docker compose run --rm -i --tty -v app:/app -e COMPOSER_PROCESS_TIMEOUT=1200 composer install
+docker compose run --rm -i --tty -v app:/app -e COMPOSER_PROCESS_TIMEOUT=1200 composer install --no-interaction --ignore-platform-reqs
 mkdir -p app/web/sites/default/files
 chmod 0777 app/web/sites/default/files
+cp app/web/sites/default/default.settings.php app/web/sites/default/settings.php
+chmod 777 app/web/sites/default/settings.php
